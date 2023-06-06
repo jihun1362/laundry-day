a6631360282ee35566a9894be1b5dbb05e92f5f8
jihunback <jihun1362@nate.com>
2023-06-06 11:50:31 +0900

    feat: 백앤드 프로젝트 세팅
    - laundry_day 프로젝트 생성
    - build.gradle에 쿼리DSL 추가
    - build.gradle에 jacoco 추가
    - build.gradle에 jwt dependencies 추가
    - build.gradle에 mysql dependencies 추가
    - build.gradle에 validation dependencies 추가

---
+++ Backend/laundry_day/src/main/java/com/meta/laundry_day/LaundryDayApplication.java
@@ -0,0 +1,13 @@
+package com.meta.laundry_day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaundryDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaundryDayApplication.class, args);
+	}
+
+}
