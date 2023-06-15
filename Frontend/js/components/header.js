8a2ff0ba98d7af7d2703219e65a4a50228da715f
“jjinheeWorld” <wlsgml450@naver.com>
2023-06-15 15:22:51 +0900

    feat: Vue.js를 활용한 세탁신청 관련 모달 구현 작업 - 세탁신청 페이지에서 Vue.js를 사용하여 모달 구현 완료

--- Frontend/js/components/header.js
+++ Frontend/js/components/header.js
@@ -14,14 +14,14 @@ Vue.component('app-header', {
           <li><a href="#" class="text">OUR SERVICES</a></li>
           <li><a href="#">이용방법</a></li>
           <li><a href="#">가격표</a></li>
-          <li><a href="#">세탁신청</a></li>
+          <li><a href="javascript:void(0)" @click="openModal">세탁신청</a></li>
         </ul>
         <div class="menu_list" :class="{ on: isMenuVisible }">
           <ul>
             <li><a href="#" class="text">OUR SERVICES<span class="material-symbols-outlined">navigate_next</span></a></li>
             <li><a href="#">이용방법<span class="material-symbols-outlined">navigate_next</span></a></li>
             <li><a href="#">가격표<span class="material-symbols-outlined">navigate_next</span></a></li>
-            <li><a href="#">세탁신청<span class="material-symbols-outlined">navigate_next</span></a></li>
+            <li><a href="javascript:void(0)" @click="openModal">세탁신청<span class="material-symbols-outlined">navigate_next</span></a></li>
             <li class="util">
               <ul>
                 <li><iconify-icon icon="material-symbols:help-outline"></iconify-icon><a href="#">공지사항</a> | <a href="#">자주 찾는 질문</a></li>
@@ -43,11 +43,30 @@ Vue.component('app-header', {
         </ul>
       </nav>
     </div>
+    <!-- 모달 -->
+    <div v-if="isModalVisible" class="modal">
+      <div class="modal-content">
+        <h3>꼭 확인해주세요!</h3>
+        <p>
+          <span> 버려도 되는 일회용 백</span>에 담아<br>
+          <span>런드리데이 + 세탁물 명</span>을<br>적어주세요
+        </p>
+      </div>
+      <div class="example-img"></div>
+      <div class="close-wrap">
+        <a href="javascript:void(0)" class="today-modal-close-btn" @click="hideModal">오늘 하루 보지 않음</a>
+        <a href="javascript:void(0)" class="modal-close-btn" @click="hideModal">
+          <span>Close<iconify-icon icon="heroicons:x-mark-solid"></iconify-icon></span>
+        </a>
+      </div>
+    </div>
+
   </header>
   `,
   data() {
     return {
-      isMenuVisible: false
+      isMenuVisible: false,
+      isModalVisible: false
     };
   },
   methods: {
@@ -71,10 +90,21 @@ Vue.component('app-header', {
       if (size >= 990) {
         this.isMenuVisible = false;
       }
+    },
+    openModal() {
+      // 세탁신청 페이지로 이동
+      window.location.href = '/Frontend/views/laundry-request.html';
+    },
+    hideModal() {
+      this.isModalVisible = false;
     }
   },
   mounted() {
     window.addEventListener('resize', this.checkSize);
+    // URL에 "laundry-request.html"이 포함되어 있는 경우 모달을 표시
+    if (window.location.href.includes('laundry-request.html')) {
+      this.isModalVisible = true;
+    }
   },
   beforeUnmount() {
     window.removeEventListener('resize', this.checkSize);
